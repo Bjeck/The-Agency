6c88ac4e440d0c40ec7a3011639595240218f280
Bjeck <bjarke@larsen-dk.dk>
2016-02-21 15:26:20 +0100

    more ui, search, and text setup

---
+++ The Agency/Assets/Person.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Person : MonoBehaviour {
+
+	public string pName;
+	public int ID;
+	public string job;
+	public List<Person> relations = new List<Person>();
+	public Sprite img;
+
+
+	//hobbies
+	//recent credit card transactions!!
+	//education
+	//social security number??
+	//credit card??
+	//phone number??
+
+
+}
---
+++ The Agency/Assets/TextManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class TextManager : MonoBehaviour {
+
+	public Text txt;
+	public Scrollbar scrb;
+
+	public string masterString;
+	public string toAdd;
+
+	public float del = 0.1f;
+
+	public bool addText = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(addText){
+			AddToText(toAdd, true);
+			addText = false;
+		}
+	
+	}
+
+
+
+
+	public void AddToText(string s, bool addspace){
+		if(addspace){
+			masterString += "\n";
+		}
+
+		toAdd = s;
+		StartCoroutine(RollText());
+	}
+
+
+
+
+
+	public IEnumerator RollText(){
+		int i = 0;
+		while (i< toAdd.Length) {
+
+
+			masterString += toAdd[i];
+			txt.text = masterString;
+			i++;
+			scrb.value = 0;
+			yield return new WaitForSeconds(del);
+
+		}
+
+	}
+
+
+
+}
