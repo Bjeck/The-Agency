21917780e20cd34aa528b30a0a4d3213bbd59f8b
Bjeck <bjarke@larsen-dk.dk>
2016-02-25 21:38:38 +0100

    initial work for sound and text input working!

---
+++ The Agency/Assets/RoomManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.Audio;
+
+public class RoomManager : MonoBehaviour {
+
+	public Dictionary<string,Vector3> rooms = new Dictionary<string, Vector3>();
+	public List<Button> buttons = new List<Button>();
+	//public List<AudioMixerGroup> mixerlist = new List<AudioMixerGroup>();
+	//public Dictionary<string,AudioMixerGroup> roommixers = new Dictionary<string, AudioMixer>();
+	
+	public AudioMixer roomMixer;
+
+	public Dictionary<string, AudioSource> roomAudio = new Dictionary<string,AudioSource>();
+	public List<GameObject> audioObjects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		rooms.Add("Living Room",new Vector3(0,0,2));
+		rooms.Add("Kitchen",new Vector3(8,0,2));
+		rooms.Add("Bedroom",new Vector3(0,0,11));
+		rooms.Add("Bathroom",new Vector3(9,0,14));
+
+		//roommixers.Add("Living Room",mixerlist[0]);
+		//roommixers.Add("Kitchen",mixerlist[1]);
+		//roommixers.Add("Bedroom",mixerlist[2]);
+		//roommixers.Add("Bathroom",mixerlist[3]);
+
+		//SETUP AUDIO OBJECTS
+		foreach(GameObject g in audioObjects){
+			roomAudio.Add(g.name,g.GetComponent<AudioSource>());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void ChangeRoom(Button b){
+		buttons.Find (x=>x==b).interactable = false;
+		foreach(Button bu in buttons.FindAll(x=>x!=b)){
+			bu.interactable = true;
+
+			roomMixer.SetFloat(bu.GetComponentInChildren<Text>().text,1000f);
+		}
+
+		Camera.main.transform.position = rooms[buttons.Find (x=>x==b).GetComponentInChildren<Text>().text];
+
+		//AudioMixerGroup currMix = roommixers[buttons.Find (x=>x==b).GetComponentInChildren<Text>().text];
+		roomMixer.SetFloat(buttons.Find (x=>x==b).GetComponentInChildren<Text>().text,22000);
+
+
+	}
+
+
+
+
+
+}
---
+++ The Agency/Assets/Story.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.Audio;
+
+public enum EventType {Text, Audio, StateChange}
+
+public class Event {
+	public string name;
+	public EventType type;
+	public int time;
+}
+
+public class TextEvent : Event {
+	public string text;
+	public TextEvent(string n, int t, string te){
+		type = EventType.Text;
+		name = n;
+		time = t;
+		text = te;
+	}
+}
+
+public class AudioEvent : Event {	
+	public string sound;
+	public AudioEvent(string n, int t, string au){
+		type = EventType.Audio;
+		name = n;
+		time = t;
+		sound = au;
+	}	
+}
+
+
+
+
+
+public class Story : MonoBehaviour {
+
+	public TextManager txtMan;
+	public RoomManager roomM;
+	public int tick = 0;
+	public Dictionary<string,Event> events = new Dictionary<string, Event>();
+
+	public AudioClip clip;
+	public AudioSource srrc;
+
+
+	// Use this for initialization
+	void Start () {
+		//TEST
+		TextEvent uh = new TextEvent("hello",2,"this is a text to tell you hello");
+		events.Add(uh.name,uh);
+
+		AudioEvent sink = new AudioEvent("sink",4,"BathroomSink");
+		events.Add(sink.name,sink);
+		//print ( uh.time);
+
+	//	
+		//TEST OVER
+
+
+
+
+
+		StartCoroutine(Tick());
+	}
+
+
+	public IEnumerator Tick(){
+		while(true){
+
+			List<Event> eventsToTrigger = events.Values.ToList().FindAll(x=>x.time==tick);
+			foreach(Event e in eventsToTrigger){
+				switch(e.type){
+				case EventType.Text:    
+					DoTextEvent(e as TextEvent);
+	           		break;
+	            case EventType.Audio:
+					DoAudioEvent(e as AudioEvent);
+	            	break;
+	            }
+			}
+
+
+			yield return new WaitForSeconds(1);
+			tick++;
+		}
+	}
+
+
+	public void DoTextEvent(TextEvent e){
+		txtMan.AddToText(e.text, true);
+	}
+
+	public void DoAudioEvent(AudioEvent e){
+		roomM.roomAudio[e.sound].Play();
+	}
+
+
+}
